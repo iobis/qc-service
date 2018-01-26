0b92963040590ee91a45d087d588ec8f3e54ddf6
Samuel Bosch <mail@samuelbosch.com>
2018-01-26 10:45:08 +0100

    numpy gc distance

--- service/geo.py
+++ service/geo.py
@@ -1,5 +1,8 @@
 from math import atan2, sqrt, degrees
 import numpy as np
+from math import radians, sin, cos
+
+RADIUS = 6371.009
 
 
 def get_centroid(points):
@@ -12,7 +15,26 @@ def get_centroid(points):
     center_lon = atan2(avg_y, avg_x)
     hyp = sqrt(avg_x * avg_x + avg_y * avg_y)
     center_lat = atan2(avg_z, hyp)
-    (degrees(center_lon), degrees(center_lat))
+    return degrees(center_lon), degrees(center_lat)
+
+
+def gc_distance_points(a, points):
+    lat1, lng1 = radians(a[1]), radians(a[0])
+    lat2, lng2 = np.radians(points[:, 1]), np.radians(points[:, 0])
+
+    sin_lat1, cos_lat1 = sin(lat1), cos(lat1)
+    sin_lat2, cos_lat2 = np.sin(lat2), np.cos(lat2)
+
+    delta_lng = np.subtract(lng2, lng1)
+    cos_delta_lng, sin_delta_lng = np.cos(delta_lng), np.sin(delta_lng)
+
+    d = np.arctan2(np.sqrt((cos_lat2 * sin_delta_lng) ** 2 +
+                 (cos_lat1 * sin_lat2 -
+                  sin_lat1 * cos_lat2 * cos_delta_lng) ** 2),
+                 sin_lat1 * sin_lat2 + cos_lat1 * cos_lat2 * cos_delta_lng)
+
+    return RADIUS * d
+
 
 # def get_distance_stats(position_ids, positions):
 #     def safe_distance(A, B):
@@ -28,3 +50,36 @@ def get_centroid(points):
 #     distances = [safe_distance(centroid, p) * radius for p in points]
 #     del points
 #     return (centroid.to_ewkt(),) + get_values_stats(distances)
+
+
+if __name__ == '__main__':
+    import random
+    random.seed(42)
+    xy = [(random.uniform(-180, 180), random.uniform(-90, 90)) for _ in range(1000000)]
+    centr = get_centroid(xy)
+
+    def gc_dist(a, b):
+        from math import radians, sin, cos, sqrt, atan2
+        lat1, lng1 = radians(a[1]), radians(a[0])
+        lat2, lng2 = radians(b[1]), radians(b[0])
+
+        sin_lat1, cos_lat1 = sin(lat1), cos(lat1)
+        sin_lat2, cos_lat2 = sin(lat2), cos(lat2)
+
+        delta_lng = lng2 - lng1
+        cos_delta_lng, sin_delta_lng = cos(delta_lng), sin(delta_lng)
+
+        d = atan2(sqrt((cos_lat2 * sin_delta_lng) ** 2 +
+                       (cos_lat1 * sin_lat2 -
+                        sin_lat1 * cos_lat2 * cos_delta_lng) ** 2),
+                  sin_lat1 * sin_lat2 + cos_lat1 * cos_lat2 * cos_delta_lng)
+
+        return RADIUS * d
+
+    dists = [gc_dist(centr, b) for b in xy]
+
+    import cProfile
+    cProfile.runctx('gc_distance_points(centr, np.asarray(xy))', globals(), locals())
+    cProfile.runctx('dists = [gc_dist(centr, b) for b in xy]', globals(), locals())
+
+    # print_timing(landdistance)()
