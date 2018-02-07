ac28b562128af69eac159eff96b764e6cce35fb3
Samuel Bosch <mail@samuelbosch.com>
2018-02-07 23:01:28 +0100

    fix tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -7,4 +7,5 @@ def rand_xy_tuple(n, minx=-180, maxx=180, miny=-90, maxy=90, seed=42):
 
 
 def rand_xy_list(n, minx=-180, maxx=180, miny=-90, maxy=90, seed=42):
-    return zip(rand_xy_tuple(n, minx, maxx, miny, maxy, seed))
+    x, y = rand_xy_tuple(n, minx, maxx, miny, maxy, seed)
+    return list(zip(x, y))
